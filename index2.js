5361695bd41cf185a5fffb757346c24ea3171a20
Zann-Dot <anaykarn234@gmail.com>
2026-07-15 09:50:53 +0530

    feat: Initialize server with express and connect database using connectDB

---
+++ index2.js
@@ -0,0 +1,12 @@
+import { connectDB } from "./db/db.connect";
+import express from "express";
+import cors from "cors"
+connectDB()
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
